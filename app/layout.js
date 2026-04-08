ba00d803d4dcc76882b82413375b3ae487998653
Fabien <chevallereafab@gmail.com>
2026-04-08 19:33:49 +0200

    Add openGraph Image

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,23 @@ import "./globals.css";
 export const metadata = {
   title: "La Cave du Coin",
   description: "Vins vivants pour cave vivante",
+  openGraph: {
+    title: "La Cave du Coin",
+    description:
+      "Vins vivants pour cave vivante.",
+    url: "https://la-cave-du-coin.vercel.app",
+    siteName: "La Cave du Coin",
+    images: [
+      {
+        url: "https://la-cave-du-coin.vercel.app/lacaveducoin_og.webp",
+        width: 1200,
+        height: 630,
+        alt: "La Cave du Coin",
+      },
+    ],
+    locale: "en_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
